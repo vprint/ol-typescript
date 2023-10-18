d9ad28cd080bdd4322c32c9df318c59a32ad1fba
vprint <valentin.printemps@gmail.com>
2023-10-18 22:21:06 +0200

    Initialisation WidgetToolbar

---
+++ src/components/widgets/WidgetToolbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="q-pa-md">
+    <div class="no-shadow">
+      <q-list>
+        <q-item>
+
+        </q-item>
+      </q-list>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>

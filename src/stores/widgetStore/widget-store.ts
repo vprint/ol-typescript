050d895d0fda980bceb223c95e98e67230441562
vprint <valentin.printemps@gmail.com>
2023-11-27 20:19:45 +0100

    Amélioration widgetStore
    Suppression d'attribut + fonction inutiles

--- src/stores/widgetStore/widget-store.ts
+++ src/stores/widgetStore/widget-store.ts
@@ -2,22 +2,12 @@ import { defineStore } from 'pinia';
 import { toolbarWidgets } from './enum';
 
 export const useWidgetStore = defineStore('widget', {
-  state: () => ({
-    _widgetList: toolbarWidgets,
-    _drawMode: '' as string
-  }),
-  getters: {
-    widgetList: (state) => state._widgetList,
-    drawMode: (state) => state._drawMode
-  },
 
-  actions: {
-    /**
-     * Definition du mode de dessin
-     * @param mode Mode de dessin
-     */
-    setDrawMode(mode: string): void {
-      this._drawMode = mode
-    }
-  },
+  state: () => ({
+    _widgetList: toolbarWidgets
+  }),
+
+  getters: {
+    widgetList: (state) => state._widgetList
+  }
 });

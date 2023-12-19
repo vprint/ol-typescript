6277c0ef7d236f2b3d6681ec8b4d9ef8e4a6516c
vprint <valentin.printemps@gmail.com>
2023-12-19 08:52:17 +0100

    Ajout d'un widget de modification des couches

---
+++ src/components/widgets/TuneTool/TuneTool.vue
@@ -0,0 +1,175 @@
+<template>
+  <RegularWidget>
+    <template #header>
+      Tuning
+    </template>
+    <template #component>
+      <transition-group tag="div" name="fade" class="container">
+        <div v-for="layer in sortedEditableLayers" :key="layer.get('name')">
+          <div class="layer-div">
+
+            <p class="merriweather">{{ layer.get('title') }}</p>
+
+            <div class="layer-control">
+
+              <q-checkbox v-model="layersVisibilities[layer.get('name')]" checked-icon="sym_o_visibility"
+                unchecked-icon="sym_o_visibility_off" @click="layer.setVisible(!layer.getVisible())" size="xl" />
+              <q-slider v-model="layersOpacities[layer.get('name')]" :min="0" :max="1" :step="0.01"
+                @update:model-value="value => layer.setOpacity(value as number)" :disable="!layer.getVisible()" />
+
+
+              <div class="up-down-buttons">
+                <q-btn flat square icon="sym_o_keyboard_double_arrow_up" :disable="layer.getZIndex() === maxZIndex"
+                  @click="setLayerIndex(layer, 1)" />
+                <q-btn flat square icon="sym_o_keyboard_double_arrow_down" :disable="layer.getZIndex() === minZIndex"
+                  @click="setLayerIndex(layer, -1)" />
+              </div>
+
+            </div>
+
+            <q-expansion-item label="Description" class="merriweather">
+              <q-card>
+                <q-card-section class="description-card merriweather">
+                  {{ layer.get('description') }}
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+
+          </div>
+
+          <q-separator />
+
+        </div>
+      </transition-group>
+    </template>
+  </RegularWidget>
+</template>
+
+<script setup lang="ts">
+
+import RegularWidget from '../RegularWidget/RegularWidget.vue';
+import { useMapStore } from 'src/stores/mapStore/map-store';
+import { onMounted, ref, Ref, computed } from 'vue';
+
+import { ILayersOpacities, ILayersVisibilities } from './types'
+import { Layer } from 'ol/layer';
+
+const mapStore = useMapStore()
+const editableLayers = getEditableLayers()
+const zIndexList = editableLayers.map(layer => layer.getZIndex()!)
+const maxZIndex = Math.max(...zIndexList)
+const minZIndex = Math.min(...zIndexList)
+const layersOpacities: Ref<ILayersOpacities> = ref({})
+const layersVisibilities: Ref<ILayersVisibilities> = ref({})
+
+const sortedEditableLayers = computed(() => {
+  return sortLayersByZIndex(editableLayers)
+})
+
+
+
+/**
+ * Récupération des opacités
+ */
+function getLayersOpacity(): void {
+  editableLayers.forEach(layer => {
+    layersOpacities.value[layer.get('name')] = layer.getOpacity() * 100
+  })
+}
+
+
+
+/**
+ * Récupération des paramètres de visibilité des couches
+ */
+function getLayersVisibilities(): void {
+  editableLayers.forEach(layer => {
+    layersVisibilities.value[layer.get('name')] = layer.isVisible()
+  })
+}
+
+
+
+/**
+ * Fonction de tri des couches par leurs valeurs Z-index
+ * @param layerlist Liste des couches à trier
+ */
+function sortLayersByZIndex(layerlist: Layer[]): Layer[] {
+  layerlist.sort((a, b) => b.getZIndex()! - a.getZIndex()!)
+  return layerlist
+}
+
+
+
+/**
+ * Récupération des layers paramétrables
+ */
+function getEditableLayers(): Layer[] {
+  const layers = mapStore.map?.getAllLayers()
+  return layers?.filter(layer => layer.get('editable'))!
+}
+
+
+
+/**
+ * Application du Z-index
+ * @param layer Couche cible
+ * @param index Valeur de Z-index à ajouter
+ */
+function setLayerIndex(layer: Layer, index: number): void {
+  const layerIndex = layer.getZIndex()!
+  const targetIndex = layerIndex + (index)
+
+  getLayerAtZIndex(targetIndex).forEach(l => l.setZIndex(l.getZIndex()! + index * (-1)))
+  layer.setZIndex(targetIndex)
+}
+
+
+
+/**
+ * Récupération des layers pour un Z-index donné
+ * @param index valeur de Z-index
+ */
+function getLayerAtZIndex(index: number): Layer[] {
+  return editableLayers.filter(layer => layer.getZIndex() === index)
+}
+
+
+onMounted(() => {
+  getLayersOpacity()
+  getLayersVisibilities()
+})
+
+</script>
+
+<style lang="sass" scoped>
+.layer-div
+  margin: 10px 30px 10px 30px
+
+.description-card
+  background-color: $secondary
+
+.layer-control
+  position: relative
+  display: flex
+  align-items: center
+  left: -15px
+
+.up-down-buttons
+  position: relative
+  left: 40px
+
+.fade-move,
+.fade-enter-active,
+.fade-leave-active
+  transition: all 0.4s cubic-bezier(0.55, 0, 0.1, 1)
+
+
+.fade-enter-from,
+.fade-leave-to
+  opacity: 0
+  transform: scaleY(0.01) translate(30px, 0)
+
+.fade-leave-active
+  position: absolute
+</style>
--- src/stores/widgetStore/enum.ts
+++ src/stores/widgetStore/enum.ts
@@ -1,6 +1,7 @@
 import InformationTool from '../../components/widgets/InformationTool/InformationTool.vue';
 import FeaturesTool from '../../components/widgets/FeaturesTool/FeaturesTool.vue';
 import ProcessingTool from '../../components/widgets/ProcessingTool/ProcessingTool.vue';
+import TuneTool from '../../components/widgets/TuneTool/TuneTool.vue'
 
 import { IWidgets } from './type'
 import { DefineComponent, markRaw } from 'vue'
@@ -26,5 +27,12 @@ export const toolbarWidgets: IWidgets = {
     title: 'InformationTool',
     icon: 'info',
     tooltip: 'Information'
+  },
+  TuneTool: {
+    tool: markRaw(TuneTool) as DefineComponent,
+    width: '400px',
+    title: 'TuneTool',
+    icon: 'sym_o_tune',
+    tooltip: 'Customization'
   }
 }
